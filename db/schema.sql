bc0a3b0f41c0f9e2a5b11cb155ed16ca91785da0
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2021-01-13 18:12:28 -0800

    setting up more files

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
 -- * Create the `burgers_db`.
 --    * Switch to or use the `burgers_db`.
 --    * Create a `burgers` table with these fields:
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers
+(burger_name, devoured)
+VALUES ("hanburger", false), ("cheeseburger", false), ("veggieburger", false);
+
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
